a58ab5b49a9812d0c524c7a5585cd83b4bf3454d
Doug Schallmoser <dougschallmoser@gmail.com>
2021-01-11 17:28:27 -0800

    continue building notes app by adding add, remove, list, and read commands

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,57 @@
 const chalk = require('chalk');
-
+const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
-const msg = getNotes()
-const greenMsg = chalk.red('Error!');
+// Customize yargs version
+yargs.version('1.1.0');
 
-console.log(msg);
-console.log(greenMsg);
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function(argv) {
+    console.log('Title: ' + argv.title);
+    console.log('Body: ' + argv.body)
+  }
+})
+
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function() {
+    console.log('Removing the note');
+  }
+})
+
+// Create list command
+yargs.command({
+  command: 'list',
+  describe: 'Lists all notes',
+  handler: function() {
+    console.log('Listing all the notes');
+  }
+})
+
+// Create read command
+yargs.command({
+  command: 'read',
+  describe: 'Reads a note',
+  handler: function() {
+    console.log('Reading a note');
+  }
+})
+
+yargs.parse();
