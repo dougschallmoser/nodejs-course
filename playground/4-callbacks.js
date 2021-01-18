e10d6e9f8534bd74f523be60dad56d0b812da5db
Doug Schallmoser <dougschallmoser@gmail.com>
2021-01-18 14:48:15 -0800

    build basic crud functionality with mongo db utilizing promises

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -1,43 +1,28 @@
-// setTimeout(() => {
-//   console.log('Two seconds are up')
-// }, 2000)
+// EXAMPLE:
+// const add = (num1, num2, callback) => {
+//   const sum = num1 + num2;
 
-// const names = ['Doug', 'Jen', 'Jess'];
-// const shortNames = names.filter(name => {
-//   return name.length <= 4
-// })
-
-// const geocode = (address, callback) => {
 //   setTimeout(() => {
-//     const data = {
-//       latitude: 0,
-//       longitude: 0
-//     }
-  
-//     callback(data)
+//     callback(sum)
 //   }, 2000)
-
 // }
 
-// geocode('Philadelphia', (data) => {
-//   console.log(data)
-// }) 
+// add(1, 4, (sum) => {
+//   console.log(sum) // Should print: 5
+// })
 
-// Goal: Mess around with the callback pattern
-//
-// 1. Define an add function that accepts the correct arguments
-// 2. Use setTimeout to simulate a 2 second delay
-// 3. After 2 seconds are up, call the callback function with the sum
-// 4. Test your work!
-
-const add = (num1, num2, callback) => {
-  const sum = num1 + num2;
-
+// EXAMPLE
+const doWorkCallback = (callback) => {
   setTimeout(() => {
-    callback(sum)
+    // callback('This is my error')
+    callback(undefined, [1,4,7])
   }, 2000)
 }
 
-add(1, 4, (sum) => {
-  console.log(sum) // Should print: 5
-})
+doWorkCallback((error, result) => {
+  if (error) {
+    return console.log(error)
+  }
+
+  console.log(result);
+});
