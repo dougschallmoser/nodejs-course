fa96f4c9f3e84a3b31f806d344e8395ef5b1bf75
Doug Schallmoser <dougschallmoser@gmail.com>
2021-02-28 09:58:01 -0800

    build functions to convert temperatures, test functions, add test environment variables and Jest testEnvironment config to dependencies

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,4 +1,4 @@
-const { calculateTip } = require('../src/math');
+const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit, add } = require('../src/math');
 
 test('Should calculate total with tip', () => {
   const total = calculateTip(10, 0.3);
@@ -8,4 +8,20 @@ test('Should calculate total with tip', () => {
 test('Should calculate total with default tip', () => {
   const total = calculateTip(10);
   expect(total).toBe(12);
+})
+
+test('Should convert 32 F to 0 C', () => {
+  const result = fahrenheitToCelsius(32);
+  expect(result).toBe(0);
+})
+
+test('Should convert 0 C to 32 F', () => {
+  const result = celsiusToFahrenheit(0);
+  expect(result).toBe(32);
+})
+
+// async testing
+test('Should add two numbers async/await', async () => {
+  const sum = await add(10, 11)
+  expect(sum).toBe(21)
 })
