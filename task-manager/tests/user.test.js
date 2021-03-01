a8c04c8881f8da6a2d29aaf044858fcab4cab5cf
Doug Schallmoser <dougschallmoser@gmail.com>
2021-03-01 08:51:19 -0800

    add fixture for testing photo upload, add mocks directory for testing sending emails, build test cases for patch and photo uploads

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -112,3 +112,38 @@ test('Should not delete account for unathenticated user', async () => {
     .send()
     .expect(401)
 })
+
+test('Should upload avatar image', async () => {
+  const response = await request(app)
+    .post('/users/me/avatar')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .attach('avatar', 'tests/fixtures/profile.jpg')
+    .expect(200)
+
+  // Check if user's avatar is a Buffer
+  const user = await User.findById(userOneId)
+  expect(user.avatar).toEqual(expect.any(Buffer))
+})
+
+test('Should update valid user fields', async () => {
+  await request(app)
+    .patch('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send({
+      name: 'Ava'
+    })
+    .expect(200)
+
+  const user = await User.findById(userOneId)
+  expect(user.name).toEqual('Ava')
+})
+
+test('Should not update invalid user fields', async () => {
+  await request(app)
+    .patch('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send({
+      location: 'Bellingham'
+    })
+    .expect(400)
+})
