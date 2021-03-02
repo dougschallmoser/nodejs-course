6a8e90b6887f894ea1a4a2f2a9c34b6445ed8038
Doug Schallmoser <dougschallmoser@gmail.com>
2021-03-02 09:20:09 -0800

    add additional tests for testing retrieving and deleting tasks

--- task-manager/tests/task.test.js
+++ task-manager/tests/task.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const app = require('../src/app');
 const Task = require('../src/models/Task');
-const { userOne, taskThree, populateDatabase } = require('./fixtures/db');
+const { userOne, userTwo, taskThree, populateDatabase } = require('./fixtures/db');
 
 beforeEach(populateDatabase)
 
@@ -19,7 +19,15 @@ test('Should create task for user', async () => {
   expect(task.completed).toBe(false)
 })
 
-test('Should fetch user specific tasks', async () => {
+test('Should fetch a particular user task by id', async () => {
+  const response = await request(app)
+    .get(`/tasks/${taskThree._id}`)
+    .set('Authorization', `Bearer ${userTwo.tokens[0].token}`)
+    .send()
+    .expect(200)
+})
+
+test('Should fetch user tasks', async () => {
   const response = await request(app)
     .get('/tasks')
     .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
@@ -27,6 +35,27 @@ test('Should fetch user specific tasks', async () => {
     .expect(200)
   
   expect(response.body.length).toEqual(2)
+})
+
+test('Should only fetch completed user tasks', async () => {
+  const response = await request(app)
+    .get('/tasks?completed=true')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200)
+
+  expect(response.body.length).toEqual(1)
+})
+
+test('Should delete user task', async () => {
+  await request(app)
+    .delete(`/tasks/${taskThree._id}`)
+    .set('Authorization', `Bearer ${userTwo.tokens[0].token}`)
+    .send()
+    .expect(200)
+
+  const task = await Task.findById(taskThree._id)
+  expect(task).toBeNull()
 })
 
 test('Should not allow one user to delete another users task', async () => {
@@ -38,4 +67,14 @@ test('Should not allow one user to delete another users task', async () => {
 
   const task = await Task.findById(taskThree._id)
   expect(task).not.toBeNull()
+})
+
+test('Should not create a task with invalid completed property', async () => {
+  await request(app)
+    .post('/tasks')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send({
+      completed: 'haha'
+    })
+    .expect(400)
 })
